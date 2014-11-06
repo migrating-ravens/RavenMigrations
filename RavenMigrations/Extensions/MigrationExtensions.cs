b179c8a6dd1a40f5d4782c05fd914e495cb80f97
Bruno Lopes <brunomlopes@gmail.com>
2014-11-05 18:05:18 -0800

    Timeout by default on Index patches

--- RavenMigrations/Extensions/MigrationExtensions.cs
+++ RavenMigrations/Extensions/MigrationExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using Raven.Client;
 
@@ -19,13 +20,16 @@ namespace RavenMigrations.Extensions
         }
 
         /// <summary>
-        /// Will wait until the store has completed indexing.
+        /// Will wait until the store has completed indexing a particular index, with an optional timeout.
         /// </summary>
         /// <remarks>Taken from Matt Warren's example here http://stackoverflow.com/q/10316721/2608 </remarks>
-        public static void WaitForIndexingOf(this IDocumentStore store, string indexName)
+        public static void WaitForIndexingOf(this IDocumentStore store, string indexName, TimeSpan? timeout = null)
         {
+            var startingTime = DateTime.UtcNow;
             while (store.DatabaseCommands.GetStatistics().StaleIndexes.Any(i => i == indexName))
             {
+                if (timeout != null && startingTime + timeout < DateTime.UtcNow)
+                    throw new InvalidOperationException(string.Format("Timeout occurred while waiting for indexing of '{0}'", indexName));
                 Thread.Sleep(10);
             }
         }
--- RavenMigrations/Migrations/IndexPatchMigration.cs
+++ RavenMigrations/Migrations/IndexPatchMigration.cs
@@ -6,6 +6,11 @@ namespace RavenMigrations.Migrations
 {
     public abstract class IndexPatchMigration : Migration
     {
+        protected IndexPatchMigration()
+        {
+            IndexingTimeout = TimeSpan.FromMinutes(5); 
+        }
+
         public abstract string UpPatch { get; }
         public virtual string DownPatch { get { return null; } }
 
@@ -19,11 +24,13 @@ namespace RavenMigrations.Migrations
             }
         }
 
+        protected TimeSpan IndexingTimeout { get; set; }
+
         protected abstract string Query { get; }
 
         public override void Up()
         {
-            DocumentStore.WaitForIndexingOf(IndexName);
+            DocumentStore.WaitForIndexingOf(IndexName, IndexingTimeout);
             DocumentStore.DatabaseCommands.UpdateByIndex(IndexName,
                 IndexQuery,
                 new ScriptedPatchRequest
@@ -39,7 +46,7 @@ namespace RavenMigrations.Migrations
         {
             if (string.IsNullOrWhiteSpace(DownPatch)) return;
 
-            DocumentStore.WaitForIndexingOf(IndexName);
+            DocumentStore.WaitForIndexingOf(IndexName, IndexingTimeout);
             DocumentStore.DatabaseCommands.UpdateByIndex(IndexName,
                 IndexQuery,
                 new ScriptedPatchRequest
