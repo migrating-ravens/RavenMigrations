40592d2e2e78c3c9c6ab893858781952265e368b
Bruno Lopes <brunomlopes@gmail.com>
2014-11-26 12:07:51 +0000

    Test for versions in namespace

---
+++ RavenMigrations.Tests/Migrations/v2/M1_Last.cs
@@ -0,0 +1,12 @@
+﻿using RavenMigrations.Migrations;
+
+namespace RavenMigrations.Tests.Migrations.v2
+{
+    public class M1_Last : Migration
+    {
+        public override void Up()
+        {
+          
+        }
+    }
+}
--- RavenMigrations.Tests/NameBasedMigrationCollectorTests.cs
+++ RavenMigrations.Tests/NameBasedMigrationCollectorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using RavenMigrations.Tests.Migrations.v1;
+using RavenMigrations.Tests.Migrations.v2;
 using Xunit;
 
 namespace RavenMigrations.Tests
@@ -11,7 +12,7 @@ namespace RavenMigrations.Tests
         {
             var collector = new NameBasedMigrationCollector(new DefaultMigrationResolver(),
                 () =>
-                    new[] { typeof(M1_3_Third), typeof(M_1_2_Second), typeof(M1_FirstTest), typeof(M_2_FourthTest), typeof(M11_2_FifthTest) }
+                    new[] { typeof(M1_3_Third), typeof(M_1_2_Second), typeof(M1_Last), typeof(M1_FirstTest), typeof(M_2_FourthTest), typeof(M11_2_FifthTest) }
                 );
 
             var orderedMigrations = collector.GetOrderedMigrations(new string[]{}).ToList();
@@ -20,12 +21,14 @@ namespace RavenMigrations.Tests
             Assert.Equal(typeof(M1_3_Third), orderedMigrations[2].MigrationType);
             Assert.Equal(typeof(M_2_FourthTest), orderedMigrations[3].MigrationType);
             Assert.Equal(typeof(M11_2_FifthTest), orderedMigrations[4].MigrationType);
+            Assert.Equal(typeof(M1_Last), orderedMigrations[5].MigrationType);
 
             Assert.Equal(orderedMigrations[0].Properties.Version, new MigrationVersion(1, 1));
             Assert.Equal(orderedMigrations[1].Properties.Version, new MigrationVersion(1, 1, 2));
             Assert.Equal(orderedMigrations[2].Properties.Version, new MigrationVersion(1, 1, 3));
             Assert.Equal(orderedMigrations[3].Properties.Version, new MigrationVersion(1, 2));
             Assert.Equal(orderedMigrations[4].Properties.Version, new MigrationVersion(1, 11, 2));
+            Assert.Equal(orderedMigrations[5].Properties.Version, new MigrationVersion(2, 1));
         }
     }
 }
