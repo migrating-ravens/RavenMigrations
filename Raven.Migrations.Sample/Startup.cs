3e812f8c5d513ef640bce450126fd92c5d76f3d8
Judah Gabriel Himango <judahgabriel@gmail.com>
2019-01-21 13:02:18 -0600

    Sample uses the live-test enviro.

--- Raven.Migrations.Sample/Startup.cs
+++ Raven.Migrations.Sample/Startup.cs
@@ -37,7 +37,7 @@ namespace Raven.Migrations.Sample
             var docStore = new DocumentStore
             {
                 Database = "Demo",
-                Urls = new[] { "http://localhost:8080" }
+                Urls = new[] { "http://live-test.ravendb.net" }
             };
             docStore.Initialize();
             services.AddSingleton<IDocumentStore>(docStore);
